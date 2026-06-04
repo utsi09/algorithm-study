a783354cebb4c3a8492e02c837d1c3beb012a640
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-06-04 18:50:06 +0900

    Update code.cpp

--- 개인 문제 풀이/2026/06/04/백준1436(완전탐색)/code.cpp
+++ 개인 문제 풀이/2026/06/04/백준1436(완전탐색)/code.cpp
@@ -2,38 +2,25 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-
-
 using namespace std;
-
-
 vector<int> devel;
+
 int cnt=0;
 
 void make_table(int n){
-    while(1){
+    for(;;cnt++){
         if(devel.size() == n) break;
-        cnt++;
         string tmp = to_string(cnt);
-        int tmp_cnt = 0;
-        bool is_devel = false;
-        for(int i=0; i<tmp.size(); i++){
-            if(tmp[i] == '6') tmp_cnt++;
-            else if(tmp[i] != '6') tmp_cnt = 0;
-            if(tmp_cnt == 3) is_devel = true;
+        if(tmp.find("666") != string::npos){
+            devel.push_back(cnt);
         }
-        if(is_devel) devel.push_back(cnt);
     }
 }
 
-
-
 int main(){
-    
     int n;
     cin >> n;
     make_table(n);
     cout << devel[n-1];
-
     return 0;
 }
