95dd4456e92fb6aed295a92212b19af02c2a05ec
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-06-14 15:13:23 +0900

    Update code.cpp

--- 개인 문제 풀이/2026/06/14/큰돌의 LIS O(N^2)/code.cpp
+++ 개인 문제 풀이/2026/06/14/큰돌의 LIS O(N^2)/code.cpp
@@ -13,12 +13,12 @@ int main(){
     }
     for(int i=0; i<n; i++){
         int max_value = 0;
-        for(int j=0; j<=i; j++){
-            if(max_value < a[j]){
-                max_value = a[j];
-                cnt[i]++;
+        for(int j=0; j<i; j++){
+            if(a[j] < a[i] && max_value < cnt[j]){
+                max_value = cnt[j];
             }
         }
+        cnt[i] = max_value +1;
         ret = max(ret, cnt[i]);
     }
     cout << ret;
