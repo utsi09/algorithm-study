920df0478fd2e78a36cc97f00cd14b26d9d47f44
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-06-16 19:58:29 +0900

    Update code.cpp

--- 개인 문제 풀이/2026/06/16/백준6236_용돈관리(이분탐색)_실버2/code.cpp
+++ 개인 문제 풀이/2026/06/16/백준6236_용돈관리(이분탐색)_실버2/code.cpp
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define INF 987654321
-int n,m;
+typedef long long ll;
+ll n,m;
 int a[100001];
 int ret = INF;
 
-int checker(int mid){
+bool checker(int mid){
     int idx = 1;
     int now = mid; //처음 한번 빼기
     
     for(int d=0; d<n; d++){
-        if(now >= a[d]){
-            now -= a[d];
-        }
-        else {
+        if(now < a[d]){
             idx++;
             now = mid;
-            if(now >= a[d]){
-                now -= a[d];
-            }
-            else return 2; //풀피인데 안되면 안되는거지
         }
+        now -= a[d];
     }
-    if(idx == m) return 0;
-    else if(idx < m) return 1;
-    else if(idx > m) return 2;
+    return (idx <= m);
 }
 
 int main() {
@@ -37,19 +30,13 @@ int main() {
     while(1){
         if(leftt > rightt) break;
         int mid = (leftt + rightt) / 2;
-        int tmp = checker(mid);
-        if(tmp == 0){
+        if(checker(mid)){
             ret = min(ret, mid);
             rightt = mid - 1;
             continue;
         }
-        else if(tmp == 1){
-            rightt = mid - 1;
-            continue;
-        }
-        else if(tmp == 2){
+        else{
             leftt = mid + 1;
-            continue;
         }
     }
     cout << ret;
