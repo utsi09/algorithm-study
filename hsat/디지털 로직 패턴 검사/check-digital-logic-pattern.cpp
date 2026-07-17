8580d58858321bf8c872c1018d36ed12bb326624
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-07-18 00:52:35 +0900

    Update check-digital-logic-pattern.cpp

--- hsat/디지털 로직 패턴 검사/check-digital-logic-pattern.cpp
+++ hsat/디지털 로직 패턴 검사/check-digital-logic-pattern.cpp
@@ -7,25 +7,23 @@ int k,m;
 
 
 int main(){
-    string s;
-    cin >> s;
-    cin >> k >> m;
-    unordered_map<unsigned long long,int> cnt;
-    for(int i=0; i<=s.size() - k; i++){
-        unsigned long long pwd = 0;
-        unsigned long long bi = 1 << (k - 1);
-        for(int idx=i; idx<i+k; idx++){ //k 길이 만큼 
-            int tmp = s[idx] - '0';
-            pwd = pwd * 2 + tmp;
-            bi/=2;
-        }
-        cnt[pwd]++;
-        if(cnt[pwd] >= m){
-            cout << "1\n";
-            return 0;
-        }
-    }
+	string s;
+	cin >> s;
+	cin >> k >> m;
+	unordered_map<unsigned long long,int> cnt;
+	for(int i=0; i<=s.size() - k; i++){
+		unsigned long long pwd = 0;
+		for(int idx=i; idx<i+k; idx++){ //k 길이 만큼 
+			int tmp = s[idx] - '0';
+			pwd = pwd * 2 + tmp;
+		}
+		cnt[pwd]++;
+		if(cnt[pwd] >= m){
+			cout << "1\n";
+			return 0;
+		}
+	}
 
-    cout << "0\n";
-    return 0;
+	cout << "0\n";
+	return 0;
 }
