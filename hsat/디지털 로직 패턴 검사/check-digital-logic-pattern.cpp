12b2c47af7cb8708801f91b2a1f628d2f16a52b7
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-07-18 00:50:43 +0900

    [Passed] 디지털 로직 패턴 검사 (10ms, 2MB)

---
+++ hsat/디지털 로직 패턴 검사/check-digital-logic-pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+int k,m;
+
+
+int main(){
+    string s;
+    cin >> s;
+    cin >> k >> m;
+    unordered_map<unsigned long long,int> cnt;
+    for(int i=0; i<=s.size() - k; i++){
+        unsigned long long pwd = 0;
+        unsigned long long bi = 1 << (k - 1);
+        for(int idx=i; idx<i+k; idx++){ //k 길이 만큼 
+            int tmp = s[idx] - '0';
+            pwd = pwd * 2 + tmp;
+            bi/=2;
+        }
+        cnt[pwd]++;
+        if(cnt[pwd] >= m){
+            cout << "1\n";
+            return 0;
+        }
+    }
+
+    cout << "0\n";
+    return 0;
+}
