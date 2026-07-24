d3ee47a125b6ab715c9f94315495051c03befab1
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-07-24 16:03:34 +0900

    [Passed] 꼬리잡기놀이 (3ms, 0MB)

--- samsung-sw/꼬리잡기놀이/tail-catch-play.cpp
+++ samsung-sw/꼬리잡기놀이/tail-catch-play.cpp
@@ -2,241 +2,164 @@
 using namespace std;
 int n,m,k;
 int adj[21][21];
-int visited[21][21];
-int dy[4] = {-1,1,0,0};
+vector<vector<pair<int,int>>> teams;
+int dy[4] = {-1,1,0,0}; //상하좌우
 int dx[4] = {0,0,-1,1};
+int visited[21][21];
 int score = 0;
-vector<pair<int,int>> tails; //각 팀별 꼬리들 저장
+
+
+// 팀에 누구있는지 디버깅
+void db_teams(){
+    for(auto& team : teams){
+        cout <<"--team--\n";
+        for(auto& p : team){
+            cout << p.first <<"," << p.second << "||";
+        }
+        cout << "\n";
+    }
+
+}
 
 void db_map(){
-  cout <<"--- map --- \n";
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      cout << adj[i][j] << " ";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << adj[i][j] <<" ";
+        }
+        cout <<'\n';
     }
-    cout << '\n';
-  }
-
-  cout <<"--- map --- \n";
 }
 
-pair<int,int> move_bfs(int tail_r, int tail_c){
-  int value = adj[tail_r][tail_c];
-  pair<int,int> tail_move;
-  adj[tail_r][tail_c] = 4;
-  visited[tail_r][tail_c] = 1;
-  queue<pair<int,int>> q;
-  int head_r = -1;
-  int head_c = -1;
-  bool is_ring = false;
-  q.push({tail_r, tail_c});
-  while(q.size()){
-    int ii = q.front().first;
-    int jj = q.front().second;
-    q.pop();
+//team 머리부터 꼬리까지 좌표 저장
+void init(){
+    for(auto& team : teams){
+        memset(visited,0,sizeof(visited));
+        int ii = team[0].first;
+        int jj = team[0].second;
+        visited[ii][jj] = 1;
+        while(1){
+            if(adj[ii][jj] == 3){
+                adj[ii][jj] = 1;
+                break;
+            }
+            int here = adj[ii][jj];
+            adj[ii][jj] = 1; //이제 맵에서는 사람 여부만 확인
+            for(int dd=0; dd<4; dd++){
+                int ny = ii + dy[dd];
+                int nx = jj + dx[dd];
+                if(ny<0 || nx<0 || ny>=n || nx>=n ||visited[ny][nx]) continue;
+                if(adj[ny][nx] != 0 && !(adj[ny][nx] > here + 1) ){ //팀에 넣을 곳
+                    visited[ny][nx] = 1;
+                    team.push_back({ny,nx});
+                    ii = ny;
+                    jj = nx;
+                }
+            }
+        }
+        
+    }
+
+}
+
+pair<int,int> find_space(int sr, int sc, int no_r, int no_c){
     for(int dd=0; dd<4; dd++){
-      int ny = ii + dy[dd];
-      int nx = jj + dx[dd];
-      if(ny<0 || nx<0 || ny>=n || nx>=n || visited[ny][nx] || adj[ny][nx] == 0) continue;
-      if(ii == tail_r&& jj == tail_c && adj[ny][nx] == 1){ //첫 스타트인데 1과 연결돼있으면 링 형태
-        is_ring = true;
-        continue;
-      }
-      if(adj[ny][nx] == 4){ //이동하려는 곳이 4면 스킵
-        head_r = ny; //헤드가 이동할 4 저장
-        head_c = nx;
-        continue;
-      }
-      if(value == 3){
-        tail_move.first = ny;
-        tail_move.second = nx;
-      }
-      visited[ny][nx] = 1; //방문
-      int tmp = adj[ny][nx];
-      adj[ny][nx] = value;
-      value = tmp;
-      q.push({ny,nx});
+        int ny = sr + dy[dd];
+        int nx = sc + dx[dd];
+        if(ny<0 || nx<0 || ny>=n || nx>=n || adj[ny][nx] == 0) continue;
+        if(ny == no_r && nx == no_c) continue;
+        return {ny,nx};
     }
-  }
-  if(!is_ring){
-    adj[tail_r][tail_c] = 4;
-    adj[head_r][head_c] = 1;
-  }
-  if(is_ring){
-    adj[tail_r][tail_c] = 1;
-  }
-  return tail_move;
 }
 
-// ~ 구현 15분
+
 void move_team(){
-  
-  for(pair<int,int>& tail : tails){
-    memset(visited, 0, sizeof(visited)); //팀마다 초기화
-    tail = move_bfs(tail.first, tail.second); //팀이동
-  }
-
-
-}
-
-//공맞은게 어느팀 몇번째 사람인지, 어떤 
-vector<pair<int,int>> find_team(int r, int c){
-  visited[r][c] = 1;
-  queue<pair<int,int>> q;
-  int tail_r = -1;
-  int tail_c = -1;
-  int head_r = -1;
-  int head_c = -1;
-  q.push({r, c});
-  while(q.size()){
-    int ii = q.front().first;
-    int jj = q.front().second;
-    if(adj[ii][jj] == 1){
-      head_r = ii;
-      head_c = jj;
-    }
-    else if(adj[ii][jj] == 3){
-      tail_r = ii;
-      tail_c = jj;
-    }
-    q.pop();
-    for(int dd=0; dd<4; dd++){
-      int ny = ii + dy[dd];
-      int nx = jj + dx[dd];
-      if(ny<0 || nx<0 || ny>=n || nx>=n || visited[ny][nx] || adj[ny][nx] == 0 || adj[ny][nx] == 4) continue;
-      visited[ny][nx] = visited[ii][jj] + 1;
-      q.push({ny,nx});
-    }
-  }
-  vector<pair<int,int>> ret;
-  ret.push_back({head_r, head_c});
-  ret.push_back({tail_r, tail_c});
-  return ret;
-}
-
-int get_order(int hit_r, int hit_c, int head_r, int head_c){
-    bool check[21][21] = {};
-    
-    int r = head_r;
-    int c = head_c;
-    int order = 1;
-
-    while(true){
-        if(r == hit_r && c == hit_c){
-            return order;
+    for(auto& team : teams){ 
+        auto next = find_space(team[0].first, team[0].second, team[1].first, team[1].second);
+        //맵에서 삭제
+        for(const auto& p : team){
+            adj[p.first][p.second] = 4;
         }
-        check[r][c] = true;
-        for(int d = 0; d < 4; d++){
-            int nr = r + dy[d];
-            int nc = c + dx[d];
+        //teams벡터 삽입
+        team.insert(team.begin(), next);
+        team.erase(team.end()-1);
 
-            if(nr < 0 || nc < 0 || nr >= n || nc >= n) continue;
-            if(check[nr][nc]) continue;
-
-            // 머리 다음은 반드시 몸통
-            if(adj[r][c] == 1 && adj[nr][nc] == 2){
-                r = nr;
-                c = nc;
-                order++;
-                break;
-            }
-
-            // 몸통 다음은 몸통 또는 꼬리
-            if(adj[r][c] == 2 &&
-               (adj[nr][nc] == 2 || adj[nr][nc] == 3)){
-                r = nr;
-                c = nc;
-                order++;
-                break;
-            }
+        //맵에 다시 그리기
+        for(const auto& p : team){
+            adj[p.first][p.second] = 1;
         }
     }
+
 }
 
 void move_ball(int t){
-  int turn = ((t / n)%4);
-  int ball_r = -1;
-  int ball_c = -1;
-  int i=0; int j=0;
+    int table = (t / n) % 4;
+    //cout << "table : " << table <<'\n';
+    pair<int,int> start, ball_direction;
+    if(table == 0){
+        start = {t % n, 0};
+        ball_direction = {0, 1};
+    }
+    else if(table == 1){
+        start = {n-1, t % n};
+        ball_direction = {-1, 0};
+    }
+    else if(table == 2){
+        start = {n-1 - (t%n), n-1};
+        ball_direction = {0, -1};
+    }
+    else{
+        start = {0, n-1 - (t%n)};
+        ball_direction = {1, 0};
+    }
 
-  if(turn == 0){
-    i = t % (n);
-    for(j=0; j<n; j++){
-      if(adj[i][j] != 0 && adj[i][j] < 4){ //사람이 공에 맞으면
-        ball_r = i;
-        ball_c = j;
-        break;
-      }
+    int ii = start.first;
+    int jj = start.second;
+    while(1){
+        if(ii<0 || jj<0 || ii>=n || jj>=n) break;
+        if(adj[ii][jj] == 1){ //사람 만나면
+            pair<int,int> target = {ii,jj};
+            for(auto& team : teams){
+                auto it = find(team.begin(), team.end(), target);
+                if(it != team.end()){
+                    int index = distance(team.begin(), it) + 1;
+                    score += pow(index, 2);
+                    // 방향 전환
+                    reverse(team.begin(), team.end());
+                }
+            }
+            break;
+        }
+        ii += ball_direction.first;
+        jj += ball_direction.second;
     }
-  }
-  
-  else if(turn == 1){
-    j = t % (n);
-      for(int i=n-1; i>=0; i--){
-        if(adj[i][j] != 0 && adj[i][j] < 4){ //사람이 공에 맞으면
-          ball_r = i;
-          ball_c = j;
-          break;
-      }
-    }
-  }
-  
-  else if(turn == 2){
-    i = n-1-(t % (n));
-    for(int j=n-1; j>=0; j--){
-      if(adj[i][j] != 0 && adj[i][j] < 4){ //사람이 공에 맞으면
-        ball_r = i;
-        ball_c = j;
-        break;
-      }
-    }
-  }
-
-  else if(turn == 3){
-    j = n - 1 - (t % (n));
-      for(int i=0; i<n; i++){
-        if(adj[i][j] != 0 && adj[i][j] < 4){ //사람이 공에 맞으면
-          ball_r = i;
-          ball_c = j;
-          break;
-      }
-    }
-  }
-  if(ball_r != -1 && ball_c != -1){
-    memset(visited, 0, sizeof(visited));
-    vector<pair<int,int>> ret = find_team(ball_r,ball_c);
-    int order = get_order(ball_r, ball_c, ret[0].first, ret[0].second);
-    score += order * order;
-
-    for(pair<int,int>& tail : tails){ //벡터에 적용
-      if(tail.first == ret[1].first && tail.second == ret[1].second){
-        tail.first = ret[0].first;
-        tail.second = ret[0].second;
-        break;
-      }
-    }
-    adj[ret[0].first][ret[0].second] = 3;
-    adj[ret[1].first][ret[1].second] = 1; //머리와 꼬리 바뀜
-  }
+    
 }
 
 int main(){
-  cin >> n >> m >> k;
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      cin >> adj[i][j];
-      if(adj[i][j] == 3){
-        tails.push_back({i,j});
-      }
+    cin >> n >> m >> k;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> adj[i][j];
+            if(adj[i][j] == 1){
+                vector<pair<int,int>> v;
+                v.push_back({i,j});
+                teams.push_back(v); //머리만 팀 목록에 넣기
+            }
+        }
     }
-  }
 
-  for(int t=0; t<k; t++){
-    // 1. 각 팀의 이동
-    move_team();
-    move_ball(t);
-  }
-  cout <<score;
-
-  return 0;
+    init();
+    //db_map();
+    for(int t=0; t<k; t++){
+        //1.각 팀 이동
+        //cout <<"--- turn :" << t <<"---\n";
+        move_team();
+        //db_teams();
+        //db_map();
+        move_ball(t);
+        //db_teams();
+        //db_map();
+    }
+    cout << score;
+    return 0;
 }
